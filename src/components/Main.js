bec6d50891743846c13d10a39d37dd4dbf09dfef
JLongZhao <474148750@qq.com>
2016-11-24 00:59:50 +0800

    新增图片样式

--- src/components/Main.js
+++ src/components/Main.js
@@ -17,19 +17,40 @@ imageDatas = (function(imageDataArr) {
 
 })(imageDatas);
 
-class GalleryByReactApp extends React.Component {
-  render() {
-    return (
-      	<section className="stage">
-    		<section className="img-sec">
+class ImgFigure extends React.Component {
+	render() {
+		return(
+			<figure className="img-figure">
+				<img src={this.props.data.imageUrl} alt={this.props.data.title}/>
+				<figcaption>
+					<h2 className="img-title">{this.props.data.title}</h2>
+				</figcaption>
+			</figure>
+		);
+	}
+}
 
-    		</section>
-    		<nav className="controller-nav">
-    			
-    		</nav>
-      	</section>
-    );
-  }
+class GalleryByReactApp extends React.Component {
+	
+  	render() {
+	  	var controllerUnits = [],
+	  		imgFigures = [];
+
+	  	imageDatas.forEach(function(value) {
+	  		imgFigures.push(<ImgFigure data={value}/>);
+	  	});
+
+	    return (
+	      	<section className="stage">
+	    		<section className="img-sec">
+	    			{imgFigures}
+	    		</section>
+	    		<nav className="controller-nav">
+	    			{controllerUnits}
+	    		</nav>
+	      	</section>
+	    );
+  	}
 }
 
 GalleryByReactApp.defaultProps = {
